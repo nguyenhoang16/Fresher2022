8a70afdf0c6a6dfbb2cb2f2a75e306d719269ff5
nguyenhoang16 <77724583+nguyenhoang16@users.noreply.github.com>
2022-01-12 16:25:20 +0700

    Update

--- HoangNV59/Bai1/app/src/main/java/com/example/bai1/MainActivity.kt
+++ HoangNV59/Bai1/app/src/main/java/com/example/bai1/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.bai1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.btnHello)
+        button.setOnClickListener{
+            Toast.makeText(this,"Xin chào",Toast.LENGTH_LONG).show()
+        }
     }
 }
